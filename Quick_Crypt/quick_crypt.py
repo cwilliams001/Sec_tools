eb85c66d9a37f542a72d03bc55b0bc57c73eb6ab
cwilliams <williamsct1@gmail.com>
2023-01-29 11:45:02 -0500

    Updated so a directory may be encrypted as well.

--- Quick_Crypt/quick_crypt.py
+++ Quick_Crypt/quick_crypt.py
@@ -4,7 +4,7 @@ import gnupg
 import getpass
 import readline
 import glob
-
+import tarfile
 
 
 # Check if script is being run as root
@@ -36,20 +36,32 @@ gpg = gnupg.GPG()
 action = input("[*] Enter 'e' to encrypt or 'd' to decrypt a file: ")
 
 if action != 'e' and action != 'd':
-   print("[-] Error: Invalid action. Please enter 'e' to encrypt or 'd' to decrypt.")
-   exit()
+    print("[-] Error: Invalid action. Please enter 'e' to encrypt or 'd' to decrypt.")
+    exit()
+
 
 def complete(text, state):
     return (glob.glob(text+'*')+[None])[state]
+
 
 readline.set_completer_delims(' \t\n;')
 readline.parse_and_bind("tab: complete")
 readline.set_completer(complete)
 
 
-# Prompt user for file name
-file_name = input("[*] Enter the name of the file: ")
-if not os.path.exists(file_name):
+# Prompt user for file or directory name
+file_name = input("[*] Enter the name of the file or directory: ")
+
+if os.path.isdir(file_name):
+    # Compress directory into tarball
+    with tarfile.open(file_name+'.tar.gz', mode='w:gz') as tar:
+        tar.add(file_name, arcname=os.path.basename(file_name))
+    # Update file_name variable to point to the tarball.
+    file_name += '.tar.gz'
+    if not os.path.exists(file_name):
+        print("[-] Error: Failed to compress directory.")
+        exit()
+elif not os.path.exists(file_name):
     print("[-] Error: File not found.")
     exit()
 
@@ -74,13 +86,14 @@ if action == 'e':
 
     # Encrypt file data with GPG symmetric encryption
     try:
-        encryption_result = gpg.encrypt(base64_data, symmetric='AES256', passphrase=password, recipients=None)
+        encryption_result = gpg.encrypt(
+            base64_data, symmetric='AES256', passphrase=password, recipients=None)
     except gnupg.errors.BadPassphrase:
         print("[-] Error: Invalid password.")
         exit()
 
     # Write encrypted data to file
-    with open('encrypted_file.gpg', 'wb') as f:
+    with open(file_name, 'wb') as f:
         f.write(encryption_result.data)
     print("[+] File has been encrypted.")
 
@@ -103,6 +116,14 @@ elif action == 'd':
     decoded_data = base64.b64decode(decryption_result.data)
 
     # Write decrypted data to file
-    with open('decrypted_file.txt', 'wb') as f:
+
+    with open(file_name, 'wb') as f:
         f.write(decoded_data)
-    print("[+] File has been decrypted.")
+
+    if tarfile.is_tarfile(file_name):
+        with tarfile.open(file_name, 'r:gz') as tar:
+            tar.extractall(path=os.path.dirname(file_name))
+            tar.close()
+        print("[+] File has been decrypted.")
+    else:
+        print("[-] Error: Failed to decompress directory.")
